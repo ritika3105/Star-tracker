b3dcf8c44ce99491e35a719d3270375819d4b55d
ritika3105 <ritikasharmasharma2004@gmail.com>
2025-12-23 12:22:17 +0530

    Create a1_catalog_download.py

---
+++ src/a1_catalog_download.py
@@ -0,0 +1,24 @@
+# ---- Metadata ----
+RA0_DEG  = 101.812
+DEC0_DEG = -48.565
+FOV_X_DEG = 0.429
+FOV_Y_DEG = 0.429
+MAG_LIMIT = 11.5
+
+query = f"""
+SELECT ra, dec, phot_g_mean_mag
+FROM gaiadr3.gaia_source
+WHERE
+  CONTAINS(
+    POINT('ICRS', ra, dec),
+    BOX('ICRS', {RA0_DEG}, {DEC0_DEG}, {FOV_X_DEG}, {FOV_Y_DEG})
+  ) = 1
+AND phot_g_mean_mag < {MAG_LIMIT}
+ORDER BY phot_g_mean_mag ASC
+"""
+
+job = Gaia.launch_job(query)
+gaia_df = job.get_results().to_pandas()
+
+gaia_df.to_csv("data/gaia_catalog_star_centroids_2.csv", index=False)
+print("A1 complete — GAIA stars:", len(gaia_df))
